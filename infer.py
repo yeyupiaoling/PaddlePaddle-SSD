7cdefe320bdd4e7a47128309244acaa7a0868a86
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-05-13 15:22:50 +0800

    change infer

--- infer.py
+++ infer.py
@@ -1,26 +1,36 @@
 import os
-import time
-
 import cv2
 import numpy as np
 import paddle.fluid as fluid
 from PIL import Image, ImageFont, ImageDraw
-import config
 
-if not os.path.exists(config.infer_model_path):
-    raise ValueError("The model path [%s] does not exist." % (config.infer_model_path))
 
-place = fluid.CUDAPlace(0) if config.use_gpu else fluid.CPUPlace()
+# image mean
+img_mean = 127.5
+# image std.
+img_std = 0.007843
+# data label/
+label_file = 'dataset/label_list'
+# threshold value
+nms_threshold = 0.45
+# infer model path
+infer_model_path = 'models/resnet_ssd/infer'
+# Whether use GPU to train.
+use_gpu = True
+
+if not os.path.exists(infer_model_path):
+    raise ValueError("The model path [%s] does not exist." % infer_model_path)
+
+place = fluid.CUDAPlace(0) if use_gpu else fluid.CPUPlace()
 exe = fluid.Executor(place)
 exe.run(fluid.default_startup_program())
 
 [infer_program,
  feeded_var_names,
- target_var] = fluid.io.load_inference_model(dirname=config.infer_model_path, executor=exe)
+ target_var] = fluid.io.load_inference_model(dirname=infer_model_path, executor=exe)
 
-with open(config.label_file, 'r', encoding='utf-8') as f:
+with open(label_file, 'r', encoding='utf-8') as f:
     names = f.readlines()
-confs_threshold = 0.45
 
 
 # 图像预处理
@@ -32,7 +42,7 @@ def load_image(image_path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.transpose((2, 0, 1))
     img = np.expand_dims(img, axis=0).astype(np.float32)
-    img = (img - config.img_mean) * config.img_std
+    img = (img - img_mean) * img_std
     return img
 
 
@@ -45,7 +55,7 @@ def infer(image_path):
     nmsed_out_v = np.array(nmsed_out_v)
     results = []
     for dt in nmsed_out_v:
-        if dt[1] < confs_threshold:
+        if dt[1] < nms_threshold:
             continue
         results.append(dt)
     return results
@@ -62,7 +72,6 @@ def clip_bbox(bbox):
 def draw_image(image_path, results):
     img = cv2.imread(image_path)
     w, h, c = img.shape
-    print(w, h)
     img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(img)
     for result in results:
@@ -80,7 +89,9 @@ def draw_image(image_path, results):
 
 
 if __name__ == '__main__':
-    img_path = 'dataset/images/image.jpg'
+    img_path = 'dataset/images1/000001.jpg'
     result = infer(img_path)
+    print(result)
+    draw_image(img_path, result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
